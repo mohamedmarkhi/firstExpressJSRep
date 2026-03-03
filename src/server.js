a9fbc158231d7a01e44f5a7ecd1f16e57e9b94c5
mohamedmarkhi <markhimohamed@outlook.fr>
2026-03-03 16:03:24 +0000

    users

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
-import {somme, substr, div, multip} from "./calc.js";
+// import {somme, substr, div, multip} from "./calc.js";
 import connectDB from "./config/db.js";
+import users from "./users.json" assert { type: "json" };
 
 dotenv.config();
 
@@ -17,35 +18,52 @@ app.get("/about", (req, res) => {
   res.send("About Express");
 });
 
-// sum
-app.get("/somme/:a/:b", (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-  res.send(`Result of ${a} and ${b} is ${somme(a, b)} `);
-});
+// // sum
+// app.get("/somme/:a/:b", (req, res) => {
+//   const a = parseInt(req.params.a);
+//   const b = parseInt(req.params.b);
+//   res.send(`Result of ${a} and ${b} is ${somme(a, b)} `);
+// });
 
-// sub
-app.get("/sub/:a/:b", (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-  res.send(`Result of ${a} mince ${b} is ${substr(a, b)} `);
-});
+// // sub
+// app.get("/sub/:a/:b", (req, res) => {
+//   const a = parseInt(req.params.a);
+//   const b = parseInt(req.params.b);
+//   res.send(`Result of ${a} mince ${b} is ${substr(a, b)} `);
+// });
 
-// mult 
-app.get("/mult/:a/:b", (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-  res.send(`Result of ${a} x ${b} is ${multip(a, b)} `);
-});
+// // mult 
+// app.get("/mult/:a/:b", (req, res) => {
+//   const a = parseInt(req.params.a);
+//   const b = parseInt(req.params.b);
+//   res.send(`Result of ${a} x ${b} is ${multip(a, b)} `);
+// });
 
 // div
-app.get("/div/:a/:b", (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-  res.send(`Result of ${a} divided by ${b} is ${div(a, b)} `);
+// app.get("/div/:a/:b", (req, res) => {
+//   const a = parseInt(req.params.a);
+//   const b = parseInt(req.params.b);
+//   res.send(`Result of ${a} divided by ${b} is ${div(a, b)} `);
+// });
+
+/*
+app.get("/:nom", (req, res) => {
+  res.send(`Hello ${req.params.nom} !`);
 });
 
+*/
 
+//GET ALL USERS
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+//get users 
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
+  res.json(user);
+});
 
 
 
@@ -53,4 +71,28 @@ connectDB();
 
 app.listen(8000, "0.0.0.0", () => {
   console.log("Server running on port 8000");
+});
+
+//POST
+app.post("/users", (req, res) => {
+  users.push(req.body);
+  res.json(users);
+});
+
+
+//PUT
+app.put("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
+  user.name = req.body.name;
+  user.country = req.body.country;
+  res.json(users);
+});
+
+//DELETE
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = users.findIndex(user => user.id === id);
+  users.splice(index, 1);
+  res.json(users);
 });
